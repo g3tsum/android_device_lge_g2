ad685f194dc433c334d4248d70c8d2274e6ccaee
houst0nn <houstonn@gmail.com>
2013-11-26 18:50:02 +0000

    update

--- device.mk
+++ device.mk
@@ -25,5 +25,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
         ro.telephony.default_network=10 \
         ro.cdma.home.operator.numeric=311480 \
         ro.cdma.home.operator.alpha=Verizon \
-        ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
-        persist.radio.mode_pref_nv10=1
+        ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83

78183a7a91218b467922d16ff91441760ab1eb3a
houst0nn <houstonn@gmail.com>
2013-12-28 06:10:44 +0100

    udpate network

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_PROPERTY_OVERRIDES += \
         telephony.lteOnCdmaDevice=1 \
-        ro.telephony.default_network=10 \
+        ro.telephony.default_network=8 \
         ro.cdma.home.operator.numeric=310120 \
         ro.cdma.home.operator.alpha=Sprint \
         ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83
